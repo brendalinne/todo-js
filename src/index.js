48a7687d03c8a33bf0b442a64fafafb560df4e3b
brenda <bapp3696@gmail.com>
2020-07-15 18:01:30 -0500

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 
-import { saludar } from './js/componentes.js';
 import './styles.css';
 
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
 
 
-const nombre = 'Brenda Aline';
+export const todoList = new TodoList();
 
-saludar( nombre );
+// todoList.todos.forEach(todo => crearTodoHtml( todo ) );
+todoList.todos.forEach(crearTodoHtml);
+
+// todoList.todos[0].imprimirClase();
